3330581998c05650c951ac77f895e7d1fde51888
boondoggler <cedric_bertolasio@trimble.com>
2018-03-05 17:26:49 -0700

    modified code to redraw extents after a new search

--- tests/integration/components/extents-map-test.js
+++ tests/integration/components/extents-map-test.js
@@ -1,4 +1,6 @@
-import { module, test } from 'qunit';
+import { resolve } from 'rsvp';
+import { module } from 'qunit';
+import test from 'ember-sinon-qunit/test-support/test';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
@@ -7,20 +9,18 @@ module('Integration | Component | extents-map', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+// stub the newMap() function so that a map is not constructed
+const mapService = this.owner.lookup('service:map-service');
+const stub = this.stub(mapService, 'newMap').callsFake(() => {
+  // return a promise
+  return resolve();
+});
 
-    await render(hbs`{{extents-map}}`);
+// Set any properties with this.set('myProperty', 'value');
+// Handle any actions with this.set('myAction', function(val) { ... });
 
-    assert.equal(this.element.textContent.trim(), '');
+await render(hbs`{{extents-map}}`);
 
-    // Template block usage:
-    await render(hbs`
-      {{#extents-map}}
-        template block text
-      {{/extents-map}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+assert.ok(stub.calledOnce, 'newMap was called once');
   });
 });
